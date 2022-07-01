382625bba192d4497afc015d4796be3f29482dc6
vinh@corsivalab.space <olivervinh@gmail.com>
2022-07-01 14:29:48 +0700

    define interface staff service

--- backend/API/Services/StaffService.cs
+++ backend/API/Services/StaffService.cs
@@ -1,14 +1,30 @@
 ﻿using API.Data;
 using API.Models;
 using API.Services.Base;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace API.Services
 {
-    public interface IStaffService : IBaseService<Staff> { }
+    public interface IStaffService : IBaseService<Staff> 
+    {
+        public Task<StaffObject> GetStaffByID(string id, string token);
+        public Task<StaffObject> GetAllStaff(string Token);
+    }
     public class StaffService : BaseService<Staff>,IStaffService
     {
         public StaffService(AppDbContext context) : base(context)
         {
+        }
+
+        public async Task<StaffObject> GetAllStaff(string Token)
+        {
+            return new StaffObject();
+        }
+
+        public async Task<StaffObject> GetStaffByID(string id, string token)
+        {
+            return new StaffObject();
         }
     }
 }
